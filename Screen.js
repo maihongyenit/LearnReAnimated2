90207e286ec2802ca8baf6d8f3ae69c9009c48a6
yenmh <yenmh@itsj-group.com>
2021-01-12 10:25:03 +0700

    init

--- Screen.js
+++ Screen.js
@@ -2,43 +2,95 @@ import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedStyle,
+  useDerivedValue,
   Easing,
+  withSpring,
+  withDecay,
+  withRepeat,
+  interpolate,
 } from 'react-native-reanimated';
-import {View, Button} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+  const [toggle, setToggle] = useState(undefined);
+  const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get('window').width;
+  const translateYValue = useDerivedValue(() => {
+    if (toggle === undefined) {
+      return withTiming(0);
+    }
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+    const toValue = toggle ? -windowHeight / 4 : windowHeight / 4;
+    return withTiming(toValue);
+  });
+  const translateXValue = useDerivedValue(() => {
+    if (toggle === undefined) {
+      return withTiming(0);
+    }
 
-  const style = useAnimatedStyle(() => {
+    const toValue = toggle ? windowWidth / 4 : -windowWidth / 4;
+    return withTiming(toValue);
+  });
+  const translateValue2 = useDerivedValue(() => {
+    const value = translateYValue.value;
+    return withSpring(value);
+  });
+
+  const boxAnimatedStyle = useAnimatedStyle(() => {
     return {
-      width: withTiming(randomWidth.value, config),
+      transform: [
+        {
+          translateY: translateYValue.value,
+        },
+        {
+          translateX: translateXValue.value,
+        },
+      ],
+    };
+  });
+
+  const boxAnimatedStyle2 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: translateValue2.value,
+        },
+      ],
     };
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
+    <TouchableOpacity
+      activeOpacity={1}
+      style={styles.container}
+      onPress={() => {
+        setToggle((s) => !s);
       }}>
-      <Animated.View
-        style={[
-          {width: 100, height: 80, backgroundColor: 'black', margin: 30},
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
+      <ScrollView onScroll></ScrollView>
+      <View style={styles.row}>
+        <Animated.View style={[styles.boxContainer, boxAnimatedStyle]} />
+        <Animated.View style={[styles.boxContainer, boxAnimatedStyle2]} />
+      </View>
+    </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  boxContainer: {
+    margin: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 4,
+    backgroundColor: 'blue',
+  },
+});
