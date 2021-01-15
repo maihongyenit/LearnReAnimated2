203fcbe0258693e8157e1fe845e567a8fa7d59d0
yenmh <maihongyenit@gmail.com>
2021-01-15 13:31:33 +0700

    remove button

--- components/ZingMp3Stack.js
+++ components/ZingMp3Stack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import PropTypes from 'prop-types';
 import {
   useAnimatedGestureHandler,
